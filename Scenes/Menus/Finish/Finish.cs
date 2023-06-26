eb0060f4ac8024871c74a66294e8e980bcc8ee5f
phopps <pathopps1@gmail.com>
2023-06-26 02:26:48 -0400

    created blank menu scenes and attached scripts, created audio scene and attached script, added google fonts, added input map for select, added BGM and SFX from kenney assets, used async and await for quitting game after SFX finished

---
+++ Scenes/Menus/Finish/Finish.cs
@@ -0,0 +1,9 @@
+using Godot;
+
+public class Finish : CanvasLayer
+{
+    public override void _Ready()
+    {
+        GD.Print("Finish.cs is ready.");
+    }
+}
---
+++ Scenes/Menus/Home/Home.cs
@@ -0,0 +1,9 @@
+using Godot;
+
+public class Home : CanvasLayer
+{
+    public override void _Ready()
+    {
+        GD.Print("Home.cs is ready.");
+    }
+}
